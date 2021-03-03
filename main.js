29ec8d0c39bf4b81148482dcb84f4a9bb6df5514
Andrii Kondysiuk <kondysiuka@gmail.com>
2021-03-03 02:45:47 +0100

    done

---
+++ main.js
@@ -0,0 +1,34 @@
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+const bodyRef = document.querySelector('body');
+const startRef = document.querySelector('[data-action="start"]');
+const stopRef = document.querySelector('[data-action="stop"]');
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+startRef.addEventListener('click', changeColor);
+
+function getColor() {
+  const numb = randomIntegerFromInterval(0, colors.length - 1);
+  bodyRef.style.backgroundColor = `${colors[numb]}`;
+}
+
+function changeColor() {
+  const intervalId = setInterval(() => {
+    getColor();
+    startRef.removeEventListener('click', changeColor);
+    stopRef.addEventListener('click', () => {
+      clearInterval(intervalId);
+      startRef.addEventListener('click', changeColor);
+    });
+  }, 1000);
+}
